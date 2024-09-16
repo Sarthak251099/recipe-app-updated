1e27086e761971a418dc69301869c970dbe9a5b3
Nandini Ojha <sarthak251099@gmail.com>
2024-09-17 04:15:37 +0530

    Suggest recipe endpoint added

--- app/app/urls.py
+++ app/app/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
     path('api/home/', include('home.urls')),
+    path('api/util/', include('util.urls')),
 ]
